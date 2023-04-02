b78964840f689f22bddab3e189f08cb4aa94ad9f
FreeBSD Starter Kit <128270910+FreeBSD-Starter-Kit@users.noreply.github.com>
2023-04-02 02:10:11 +0000

    Added device path
    Added a new Gtk.Entry for entering the device path, and made the width of both text fields to 50 characters using the width_chars parameter.

--- imagewriter.py
+++ imagewriter.py
@@ -3,43 +3,47 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import subprocess
 
-
 class IMGWriter(Gtk.Window):
 
     def __init__(self):
-        super().__init__()
+        Gtk.Window.__init__(self, title="IMGWriter")
 
-        self.vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(self.vbox)
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
 
         label = Gtk.Label(label="Enter the path to the ISO image:")
-        self.vbox.pack_start(label, True, True, 0)
+        vbox.pack_start(label, True, True, 0)
 
-        self.entry = Gtk.Entry()
-        self.vbox.pack_start(self.entry, True, True, 0)
+        self.entry = Gtk.Entry(width_chars=50)
+        vbox.pack_start(self.entry, True, True, 0)
+
+        device_label = Gtk.Label(label="Enter the device (e.g. /dev/da0):")
+        vbox.pack_start(device_label, True, True, 0)
+
+        self.device_entry = Gtk.Entry(width_chars=50)
+        vbox.pack_start(self.device_entry, True, True, 0)
 
         button = Gtk.Button(label="Write to USB")
         button.connect("clicked", self.write_to_usb)
-        self.vbox.pack_start(button, True, True, 0)
-
-        self.connect("delete-event", Gtk.main_quit)
+        vbox.pack_start(button, True, True, 0)
 
     def write_to_usb(self, widget):
         iso_path = self.entry.get_text()
-        command = f"dd if={iso_path} of=/dev/da0 bs=1M"
-        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = proc.communicate()
-        if proc.returncode == 0:
-            message = "The ISO image was successfully written to the USB drive."
-            message_type = Gtk.MessageType.INFO
+        device_path = self.device_entry.get_text()
+        command = f"dd if={iso_path} of={device_path} bs=1M"
+        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if result.returncode == 0:
+            dialog = Gtk.MessageDialog(parent=self, flags=0, message_type=Gtk.MessageType.INFO, buttons=Gtk.ButtonsType.OK, text="The ISO image was successfully written to the USB drive.")
         else:
-            message = "An error occurred while writing the ISO image to the USB drive."
-            message_type = Gtk.MessageType.ERROR
-        with Gtk.MessageDialog(parent=self, flags=0, message_type=message_type,
-                               buttons=Gtk.ButtonsType.OK, text=message) as dialog:
-            dialog.run()
+            dialog = Gtk.MessageDialog(parent=self, flags=0, message_type=Gtk.MessageType.ERROR, buttons=Gtk.ButtonsType.OK, text="An error occurred while writing the ISO image to the USB drive.")
+        dialog.run()
+        dialog.destroy()
+
+    def on_delete_event(self, widget, event):
+        Gtk.main_quit()
 
 if __name__ == "__main__":
     window = IMGWriter()
+    window.connect("delete-event", window.on_delete_event)
     window.show_all()
     Gtk.main()

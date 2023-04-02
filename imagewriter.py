03d9ddcc13a855de3be85430fd3409b531c4d5cc
FreeBSD Starter Kit <128270910+FreeBSD-Starter-Kit@users.noreply.github.com>
2023-04-02 01:48:36 +0000

    Update imagewriter.py
    Replaced the message_format keyword argument in the Gtk.MessageDialog constructor with the recommended text argument.
    
    Replaced the positional argument in the Gtk.Label constructor with the keyword argument label. This removes the PyGTKDeprecationWarning. Similarly, passed the parent, flags, message_type, buttons, and text arguments to the Gtk.MessageDialog constructor using keyword arguments to avoid any such warnings.

--- imagewriter.py
+++ imagewriter.py
@@ -3,42 +3,43 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import subprocess
 
+
 class IMGWriter(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title="Image Writer")
-        self.set_default_size(400, 200)
+        super().__init__()
 
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)
+        self.vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(self.vbox)
 
-        label = Gtk.Label("Enter the path to the ISO image:")
-        vbox.pack_start(label, True, True, 0)
+        label = Gtk.Label(label="Enter the path to the ISO image:")
+        self.vbox.pack_start(label, True, True, 0)
 
         self.entry = Gtk.Entry()
-        vbox.pack_start(self.entry, True, True, 0)
+        self.vbox.pack_start(self.entry, True, True, 0)
 
         button = Gtk.Button(label="Write to USB")
         button.connect("clicked", self.write_to_usb)
-        vbox.pack_start(button, True, True, 0)
+        self.vbox.pack_start(button, True, True, 0)
+
+        self.connect("delete-event", Gtk.main_quit)
 
     def write_to_usb(self, widget):
         iso_path = self.entry.get_text()
-        command = "dd if={} of=/dev/da0 bs=1M".format(iso_path)
-        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if result.returncode == 0:
-            dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.INFO, Gtk.ButtonsType.OK, "The ISO image was successfully written to the USB drive.")
+        command = f"dd if={iso_path} of=/dev/da0 bs=1M"
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = proc.communicate()
+        if proc.returncode == 0:
+            message = "The ISO image was successfully written to the USB drive."
+            message_type = Gtk.MessageType.INFO
         else:
-            dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.ERROR, Gtk.ButtonsType.OK, "An error occurred while writing the ISO image to the USB drive.")
-        dialog.run()
-        dialog.destroy()
-
-    def on_delete_event(self, widget, event):
-        Gtk.main_quit()
+            message = "An error occurred while writing the ISO image to the USB drive."
+            message_type = Gtk.MessageType.ERROR
+        with Gtk.MessageDialog(parent=self, flags=0, message_type=message_type,
+                               buttons=Gtk.ButtonsType.OK, text=message) as dialog:
+            dialog.run()
 
 if __name__ == "__main__":
     window = IMGWriter()
-    window.connect("delete-event", window.on_delete_event)
     window.show_all()
     Gtk.main()
-
